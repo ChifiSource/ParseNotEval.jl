fef83953eae14b5a1110b715aa262f67cc01ba80
emmy <52672675+emmettgb@users.noreply.github.com>
2022-04-25 03:45:05 -0400

    everything except dict/datetime :)

--- src/ParseNotEval.jl
+++ src/ParseNotEval.jl
@@ -1,6 +1,6 @@
 module ParseNotEval
 __precompile__()
-using Dates: Date
+using Dates
 import Base: parse
 """
 - ParseNotEval.jl
@@ -15,7 +15,7 @@ parse(type)
 ```
 """
 function parse(T::Type{Any}, s::AbstractString)
-    x = replace(s, " " => "")
+    x::String = replace(s, " " => "")
     if contains(x, ".")
         try
             x = parse(Float64, x)
@@ -46,10 +46,11 @@ function parse(T::Type{Any}, s::AbstractString)
         # Pair
     elseif contains(x, "=>")
         try
-            parse(Pair, x)
+            x = parse(Pair, x)
         catch
             return(x)
         end
+        return(x)
         # Bool
     elseif contains(x, "true") || contains(x, "false")
         x = replace(x, " " => "")
@@ -58,6 +59,7 @@ function parse(T::Type{Any}, s::AbstractString)
         catch
             return(x)
         end
+        return(x)
         # Integers/Date/String
     else
         try
@@ -73,20 +75,33 @@ function parse(T::Type{Any}, s::AbstractString)
     end
 end
 """
-
+### parse(T::Type{Array}, s::AbstractString) -> Array{typeof(parse(Any, **s**))}
+Parses **s** into type **T**.
+**arguments**
+- T <: DataType; type to parse **s** into.
+- s <: AbstractString; string to be parsed into type **T**.
+----------------
+### example
+```
+example_input = "[5, 10, 15, 20]"
+my_array::Array{Int64} = parse(Array, example_input)
+[5, 10, 15, 20]
+my_array::Array{Float64} = parse(Float64, my_array)
+[5.0, 10.0, 15.0, 20.0]
+```
 """
 function parse(T::Type{Array}, s::AbstractString)
     println("calling parse array")
     if ~(contains(s, "["))
         throw(ErrorException("$s not parsable as $T !");)
     end
-    s = replace(s, "[" => "")
+    s::String = replace(s, "[" => "")
     s = replace(s, "]" => "")
     s = replace(s, " " => "")
-    dims = split(s, ",")
-    T = typeof(parse(Any, string(dims[1]))) # <- we check type now by parsing any,
+    dims::Array = split(s, ",")
+    T::DataType = typeof(parse(Any, string(dims[1]))) # <- we check type now by parsing any,
 # so that way we can parse as something instead of as Any (waaaay faster.)
-    [try; parse(T, d) catch; nothing end for d in dims]
+    [try; parse(T, d) catch; nothing end for d in dims]::Vector
 end
 """
 
@@ -96,20 +111,56 @@ parse(T::Type{String}, s::AbstractString) = string(s)
 
 """
 function parse(T::Type{Dict}, s::AbstractString)
-    s = replace(s, "{" => "")
-    s = replace(s, "}" => "")
-    s = replace(s, " " => "")
-    # Json formatted dict
+    s::String = replace(s, " " => "")
+    structures = Dict()
+    # For Json formatted dict
     if contains(s, ":")
         s = replace(s, ":" => "=>")
-    # Julia formatted dict
     end
-    valsnkeys = split(s, "=>")
-    vals = valsnkeys[2:2:length(valsnkeys)]
-    keys = valsnkeys[1:2:length(valsnkeys)]
+    # We need to make sure there are no arrays/dicts before we separate
+    # pairs by ,
+    points = findall("=>", s)
+    points = [p[1] for p in points]
+    if length(findall("{", s)) == 1
+        # Checks for dictionaries, marks them
+        s = replace(s, "{" => "")
+        s = replace(s, "}" => "")
+    else
+        for pos in findall("{", s)
+            pos = pos[1]
+            if pos != 1
+                close = findnext("}", s, pos)[1]
+                println(pos:close)
+                for point in 1:length(points)
+                    if point != length(points) - 1
+                        if point == length(points)
+                            if close in Array(points[point]:points[point + 1])
+                                push!(point => parse(Dict, s[pos:close]))
+                                s = replace(s, s[pos:close] => "")
+                            end
+                        end
+                    end
+                    end
+                end
+            else
+
+            end
+
+        end
+    end
+    if contains(s, "[")
+
+    end
     newdct = Dict()
-    for (key, val) in zip(keys, vals)
-        push!(newdct, parse(Symbol, key) => parse(Any, val))
+    valsnkeys = split(s, ",")
+    for val in 1:length(valsnkeys)
+        keyval = split(valsnkeys[val], "=>")
+        if val in keys(structures)
+            push!(newdct,
+             parse(Symbol, valsnkeys[val][1]) => parse(Any, structures[val]))
+        else
+            push!(newdct, parse(Symbol, keyval[1]) => parse(Any, keyval[2]))
+        end
     end
     newdct
 end
@@ -118,6 +169,18 @@ parse(T::Type{Symbol}, s::AbstractString) = Symbol(s)
 
 function parse(T::Type{Date}, s::AbstractString)
     # 2013-07-01
+    s = split(s, "-")
+    if length(s) > 3
+        throw(" Too many elements for a Date.")
+    end
+    y::Year = Dates.Year(parse(Int64, s[1]))
+    m::Month = Dates.Month(parse(Int64, s[2]))
+    d::Day = Dates.Day(parse(Int64, s[3]))
+    Date(y, m, d)::Date
+end
+
+function parse(T::Type{DateTime}, s::AbstractString)
+    
 end
 
 function parse(T::Type{Pair}, s::AbstractString)
@@ -129,7 +192,8 @@ end
 function parse(T::Type{missing})
     return(missing)
 end
-
+"""
+"""
 parse(type::Type, x::Array) = [parse(type, val) for val in x]
 parse(type::Type, x::Pair) = x[1] => parse(type, x[2])
 
